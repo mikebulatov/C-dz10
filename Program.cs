196c28775b775fdc51ad4e6d75faba9518ae499b
Mikebulatov <mikebulatov@yandex.ru>
2023-12-30 13:18:12 +0700

    start

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.");
+
+
+void PrintNum(int start)
+{
+  if(start>0)
+  {
+  Console.Write("{0:D} ", start);
+  PrintNum(start-1);
+  } 
+}
+
+Console.Write("Введите число N:");
+PrintNum(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine();
